d84b0ca4d42f9a13909a180fb0204f94802de508
Dipronil1998 <68066910+Dipronil1998@users.noreply.github.com>
2021-04-19 20:12:09 +0530

    Update settings.py

--- poll/settings.py
+++ poll/settings.py
@@ -132,5 +132,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dipronildas8961228208@gmail.com'
-EMAIL_HOST_PASSWORD = '13#12#2**6'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
